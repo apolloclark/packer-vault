2504ca6948b6fe5fedadfde0ddcc855f2a91aff4
apolloclark <apolloclark@gmail.com>
2019-09-14 17:39:51 -0400

    refactored script to be compatible with Docker RUN format

--- base/scripts/install_basics.sh
+++ base/scripts/install_basics.sh
@@ -23,39 +23,39 @@ elif [ -x "$(command -v yum)" ]; then
     yum makecache fast
     yum update -y
     yum install -y which nano curl wget git gnupg2 initscripts hostname gzip \
-        tar gzip shadow-utils
+        tar unzip shadow-utils
     yum clean all
     python --version
 fi
 
 
 
-# install the systemctl stub
-cd /tmp
-curl -sLO https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl.py \
+# https://github.com/gdraheim/docker-systemctl-replacement
+cd /tmp \
+  && curl -sLO https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl.py \
   && yes | cp -f systemctl.py /usr/bin/systemctl \
   && chmod a+x /usr/bin/systemctl \
   && mkdir -p /run/systemd/system/ \
   && rm -rf /sbin/initctl
 
 # https://github.com/Yelp/dumb-init
-wget -q -O /usr/bin/dumb-init https://github.com/Yelp/dumb-init/releases/download/v1.2.2/dumb-init_1.2.2_amd64 \
+DUMBINIT_VERSION=1.2.2 \
+  && wget -q -O /usr/bin/dumb-init "https://github.com/Yelp/dumb-init/releases/download/v${DUMBINIT_VERSION}/dumb-init_${DUMBINIT_VERSION}_amd64" \
   && chmod +x /usr/bin/dumb-init
 
-# gosu
-# https://github.com/tianon/gosu/releases
+# https://github.com/tianon/gosu
 # https://gist.github.com/rafaeltuelho/6b29827a9337f06160a9
 # https://cinhtau.net/2017/06/19/install-gosu-for-docker/
-export GOSU_VERSION=1.11
-gpg --keyserver ha.pool.sks-keyservers.net --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4 \
-    && curl -o /usr/bin/gosu -sSL "https://github.com/tianon/gosu/releases/download/${GOSU_VERSION}/gosu-amd64" \
-    && curl -o /usr/bin/gosu.asc -SL "https://github.com/tianon/gosu/releases/download/${GOSU_VERSION}/gosu-amd64.asc" \
-    && gpg --batch --verify /usr/bin/gosu.asc /usr/bin/gosu \
-    && rm /usr/bin/gosu.asc \
-    && rm -rf /root/.gnupg/ \
-    && chmod +x /usr/bin/gosu \
-    && gosu nobody true
+GOSU_VERSION=1.11 \
+  && gpg --keyserver ha.pool.sks-keyservers.net --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4 \
+  && curl -o /usr/bin/gosu -sSL "https://github.com/tianon/gosu/releases/download/${GOSU_VERSION}/gosu-amd64" \
+  && curl -o /usr/bin/gosu.asc -SL "https://github.com/tianon/gosu/releases/download/${GOSU_VERSION}/gosu-amd64.asc" \
+  && gpg --batch --verify /usr/bin/gosu.asc /usr/bin/gosu \
+  && rm /usr/bin/gosu.asc \
+  && rm -rf /root/.gnupg/ \
+  && chmod +x /usr/bin/gosu \
+  && gosu nobody true
 
 # create vault user and group
-groupadd vault
-useradd -g vault vault
+groupadd vault \
+  && useradd -g vault vault
